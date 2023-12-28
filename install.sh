958a4697a34957b8579fcc6ce745ced5edf923cf
v1nch3r <musarrum@gmail.com>
2023-12-28 20:57:39 +0700

    update

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ sudo apt-get install speedtest -y
 
 #install marzban
 sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
+marzban down
 
 #install cert
 curl https://get.acme.sh | sh -s email=$email
